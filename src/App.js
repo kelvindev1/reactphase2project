91214bfed47df7a542be311c3e7f80132fc73d27
kelvindev1 <kelvinmutugi336@gmail.com>
2024-05-07 01:27:53 +0300

    modifies the component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Hero from "./components/Hero";
+import Homecard from "./components/Homecard";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Homecard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Homecard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "./Card";
+function Homecard() {
+  return (
+    <div>
+      <Card>
+        <div className="card">
+          <div className="card-body">
+            {" "}
+            <h5 className="card-title">For Developers</h5>
+            <p className="card-text">
+              Browse our React jobs and start your career today
+            </p>
+            <a href="#" className="btn btn-primary">
+              Browse jobs
+            </a>
+          </div>
+        </div>
+      </Card>
+
+      <Card>
+        {" "}
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">For Employers</h5>
+            <p className="card-text">
+              List your job to find the perfect developer for the role
+            </p>
+            <a href="#" className="btn btn-primary">
+              Add Job
+            </a>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default Homecard;
