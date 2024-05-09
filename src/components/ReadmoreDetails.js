6733d9139fcc4bd1676ac3c722fc9f2f0aec114f
ernestmusau <ernest.musau@student.moringaschool.com>
2024-05-09 10:08:40 +0300

    changes on ReadMoreDetails

--- src/components/ReadmoreDetails.js
+++ src/components/ReadmoreDetails.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function ReadmoreDetails() {
   const [jobs, setJobs] = useState([]);
+  const [selectedJobId, setSelectedJobId] = useState(null);
 
   useEffect(() => {
     // Fetch data from db.json using fetch API
@@ -15,18 +16,26 @@ function ReadmoreDetails() {
       });
   }, []);
 
+  const handleJobClick = (jobId) => {
+    setSelectedJobId(jobId === selectedJobId ? null : jobId); // Toggle selected job
+  };
+
   return (
     <div>
       {jobs.map((job) => (
         <div key={job.id} className="job-details">
-          <h2>{job.title}</h2>
-          <p>{job.description}</p>
-          <p>Type: {job.type}</p>
-          <p>Location: {job.location}</p>
-          <p>Salary: {job.salary} / Year</p>
-          <p>Company: {job.company.name}</p>
-          <p>Contact Email: {job.company.contactEmail}</p>
-          <p>Contact Phone: {job.company.contactPhone}</p>
+          <h2 onClick={() => handleJobClick(job.id)}>{job.title}</h2>
+          {selectedJobId === job.id && (
+            <div>
+              <p>{job.description}</p>
+              <p>Type: {job.type}</p>
+              <p>Location: {job.location}</p>
+              <p>Salary: {job.salary} / Year</p>
+              <p>Company: {job.company.name}</p>
+              <p>Contact Email: {job.company.contactEmail}</p>
+              <p>Contact Phone: {job.company.contactPhone}</p>
+            </div>
+          )}
         </div>
       ))}
     </div>
