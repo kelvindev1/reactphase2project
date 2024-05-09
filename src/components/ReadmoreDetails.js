b62ac8ef9bcee6690035bc09545bdf0173612244
kelvindev1 <kelvinmutugi336@gmail.com>
2024-05-09 13:06:36 +0300

    rectifies error on ReadmoreDetails to dispay data when link is clicked

--- src/components/ReadmoreDetails.js
+++ src/components/ReadmoreDetails.js
@@ -7,10 +7,16 @@ function ReadmoreDetails() {
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`http://localhost:3000/jobs${id}`)
-      .then((response) => response.json())
+    fetch(`http://localhost:3000/jobs/${id}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+      })
       .then((data) => {
-        setJobs(data);
+        setJobs([data]);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -19,18 +25,20 @@ function ReadmoreDetails() {
 
   return (
     <div>
-      {jobs.map((job) => (
-        <div key={job.id} className="job-details">
-          <h2>{job.title}</h2>
-          <p>{job.description}</p>
-          <p>Type: {job.type}</p>
-          <p>Location: {job.location}</p>
-          <p>Salary: {job.salary} / Year</p>
-          <p>Company: {job.company.name}</p>
-          <p>Contact Email: {job.company.contactEmail}</p>
-          <p>Contact Phone: {job.company.contactPhone}</p>
-        </div>
-      ))}
+      {Array.isArray(jobs) &&
+        jobs.map((job) => (
+          <div key={job.id} className="job-details">
+            <h2>{job.title}</h2>
+            <p>{job.description}</p>
+            <p>Type: {job.type}</p>
+            <p>Location: {job.location}</p>
+            <p>Salary: {job.salary} / Year</p>
+            <p>Company: {job.company.name}</p>
+            <p>Contact Email: {job.company.contactEmail}</p>
+            <p>Contact Phone: {job.company.contactPhone}</p>
+          </div>
+        ))}
+      {!Array.isArray(jobs) && <div>No jobs found</div>}
     </div>
   );
 }
