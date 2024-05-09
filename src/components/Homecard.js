9192d416b094c1998f681fc56711e2770ff90d9a
kelvindev1 <kelvinmutugi336@gmail.com>
2024-05-09 03:30:43 +0300

    styles

--- src/components/Homecard.js
+++ src/components/Homecard.js
@@ -5,7 +5,7 @@ function Homecard() {
   return (
     <div>
       <Card>
-        <div className="card">
+        <div className="card" style={{ margin: "2rem" }}>
           <div className="card-body">
             {" "}
             <h5 className="card-title">For Developers</h5>
@@ -21,7 +21,7 @@ function Homecard() {
 
       <Card>
         {" "}
-        <div className="card">
+        <div className="card" style={{ margin: "2rem" }}>
           <div className="card-body">
             <h5 className="card-title">For Employers</h5>
             <p className="card-text">
